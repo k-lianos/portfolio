9d37e9ef75f5e69069d632b9029ecad7785b86d7
Konstantinos Lianos <konstantinos.lianos@gmail.com>
2025-02-02 08:32:56 +0200

    Adds a new template tag for importing svgs in the HTMLs

--- build.js
+++ build.js
@@ -1,8 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define source and output folders
 const partialsFolder = path.join(__dirname, 'partials');
+const svgFolder = path.join(__dirname, 'svg');
 const distFolder = path.join(__dirname, 'docs');
 
 // Ensure the dist folder exists
@@ -29,12 +34,27 @@ const combineHTML = () => {
 			});
 		}
 
+		console.log(`Combined HTML`);
+		return template;
+
 		// Write combined HTML to output
-		fs.writeFileSync(path.join(distFolder, 'index.html'), template, 'utf-8');
-		console.log(`Combined HTML written to ${path.join(distFolder, 'index.html')}`);
 	} catch (err) {
 		console.error('Error combining HTML:', err);
 	}
+};
+
+const replaceSvgTags = template => {
+	template = template.replace(/@@svg\(['"](.+?)['"]\)/g, (match, fileName) => {
+		const filePath = path.join(svgFolder, `${fileName}.svg`);
+		if (fs.existsSync(filePath)) {
+			return fs.readFileSync(filePath, 'utf-8');
+		} else {
+			console.error(`File not found: ${fileName}`);
+			return '';
+		}
+	});
+	console.log(`Replaced all SVG tags`);
+	return template;
 };
 
 const combineCSS = () => {
@@ -52,5 +72,7 @@ const combineCSS = () => {
 };
 
 // Run the combining function
-combineHTML();
+let template = combineHTML();
+template = replaceSvgTags(template);
+fs.writeFileSync(path.join(distFolder, 'index.html'), template, 'utf-8');
 combineCSS();
