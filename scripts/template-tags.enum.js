cd3e87ed2ab3ff463dfce02aa4989243d08dfdc5
Konstantinos Lianos <konstantinos.lianos@gmail.com>
2025-02-03 16:25:39 +0200

    Adds on git's pre-commit hook, a code beautification step

--- scripts/template-tags.enum.js
+++ scripts/template-tags.enum.js
@@ -1,5 +1,5 @@
 export const TemplateTags = Object.freeze({
-    Partial: Symbol('@@partial'),
-    Svg: Symbol('@@svg'),
-    Style: Symbol('@@style'),
+  Partial: Symbol('@@partial'),
+  Svg: Symbol('@@svg'),
+  Style: Symbol('@@style'),
 });
